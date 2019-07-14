5f2a8349f9bf5f61350e9ea2a7c4be340c9b9296
Brandon Rowe <brandonhrowe@gmail.com>
2019-07-14 14:32:38 -0400

    model created

--- api/models.py
+++ api/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
+from django.db.models.aggregates import Count
+from random import randint
 
 # Create your models here.
+class FilmManager(models.Manager):
+  def get_random_film(self):
+    count = self.aggregate(count=Count('id'))['count']
+    random_index = randint(0, count - 1)
+    return self.all()[random_index]
+
+class Film(models.Model):
+  title = models.CharField(max_length=255)
+  timecodes = ArrayField(models.CharField(max_length=10))
+  runtime = models.CharField(max_length=10)
+  tags = ArrayField(models.CharField(max_length=255, blank=True))
+  collection = models.CharField(max_length=100)
+
+  objects = models.Manager()
+  random_film = FilmManager()
+
+  def __str__(self):
+    return self.title
+
+
