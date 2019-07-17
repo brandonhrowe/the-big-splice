22d0f860e18fb127e53c73c91c70b96b953f0f31
Brandon Rowe <brandonhrowe@gmail.com>
2019-07-17 16:58:20 -0400

    sorting of clips working

--- the_big_splice_frontend/src/components/Thumbnails.js
+++ the_big_splice_frontend/src/components/Thumbnails.js
@@ -1,37 +1,25 @@
-import React from 'react'
-import {SortableContainer, SortableElement} from 'react-sortable-hoc'
+import React from "react";
+import { SortableContainer, SortableElement } from "react-sortable-hoc";
 
-const ThumbnailItem = SortableElement(({clip}) => <img src={`/static/${clip}_Thumbnail.jpg`} alt={`thumbnail_${clip}`} />)
+const ThumbnailItem = SortableElement(({ clip }) => (
+  <div>
+    <img src={`/static/${clip}_Thumbnail.jpg`} alt={`thumbnail_${clip}`} />
+  </div>
+));
 
-const ThumbnailContainer = SortableContainer(({children}) => {
-  return (
-    <div className="thumbnails">
-      {children}
-    </div>
-  )
-})
-
-// {clips.map((clip, index) => (
-//   <ThumbnailItem key={index} index={index} clip={clip}/>
-// ))}
+const ThumbnailContainer = SortableContainer(({ children }) => {
+  return <div className="thumbnails">{children}</div>;
+});
 
 const Thumbnails = props => {
-  const {clips, onSortEnd} = props
+  const { clips, onSortEnd } = props;
   return (
-    <ThumbnailContainer onSortEnd={onSortEnd}>
+    <ThumbnailContainer onSortEnd={onSortEnd} axis="x">
       {clips.map((clip, index) => (
-        <ThumbnailItem key={index} index={index} clip={clip}/>
+        <ThumbnailItem key={index} index={index} clip={clip} />
       ))}
     </ThumbnailContainer>
+  );
+};
 
-    // <div className="thumbnails">
-    //   {clips.map((clip, index) => (
-    //     <div key={index}>
-    //       <img src={`/static/${clip}_Thumbnail.jpg`} alt={`thumbnail_${clip}`} />
-    //     </div>
-    //   ))}
-    // </div>
-  )
-}
-
-export default Thumbnails
+export default Thumbnails;
