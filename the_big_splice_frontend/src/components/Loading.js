508d1829e75fdcf6c6ac305dd89c1f90af710433
Brandon Rowe <brandonhrowe@gmail.com>
2019-07-19 11:09:21 -0400

    loading page properly toggles svg and gif

--- the_big_splice_frontend/src/components/Loading.js
+++ the_big_splice_frontend/src/components/Loading.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import logo from "../BigSplice_icon.svg";
-import clock from "../Clock_TheStranger_256x192.gif";
+import clock from "../Clock_TheStranger_640x480.gif";
 
 export default class Loading extends Component {
   constructor() {
@@ -10,26 +10,37 @@ export default class Loading extends Component {
     };
   }
 
-  // componentDidMount() {
-  //   setTimeout(() => {
-  //     this.setState({
-  //       displayGif: true
-  //     });
-  //   }, 10000);
-  // }
+  componentDidMount() {
+    setInterval(() => {
+      this.setState((prevState) => ({
+        displayGif: !prevState.displayGif
+      }));
+    }, 15000);
+  }
 
   render() {
     const { displayGif } = this.state;
     return (
-      <div>
+      <div className="loading-container">
         <h1 className="title">Loading...</h1>
-        <div className={`gif-container ${displayGif && "visible"}`}>
+        <h3>This could take a minute or so. Please don't detour away!</h3>
+        {/* <div className={`gif-container ${displayGif && "visible"}`}>
           <div>
-            <img src={clock} alt="The Stranger Clock" />
           </div>
           <h3>Don't be a stranger, hang around for a while.</h3>
+        </div> */}
+        <div className="image-container">
+          <img
+            src={clock}
+            className={`${displayGif && "visible"} gif`}
+            alt="The Stranger Clock"
+          />
+          <img
+            src={logo}
+            className={`${!displayGif && "visible"} logo bottom`}
+            alt="logo"
+          />
         </div>
-        <img src={logo} className="logo" alt="logo" />
       </div>
     );
   }
